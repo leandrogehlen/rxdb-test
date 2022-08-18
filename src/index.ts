09c5b966808c8b016fc31bfede780c009c6fccbe
Leandro Guindani Gehlen <leandrogehlen@gmail.com>
2022-08-18 08:57:46 -0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { createRxDatabase } from "rxdb";
+import { replicateRxCollection } from "rxdb/plugins/replication";
 import { getRxStorageDexie } from "rxdb/plugins/dexie";
 
 const contactSchema = {
@@ -33,4 +34,25 @@ export async function initDatabase() {
   return database;
 }
 
-initDatabase();
+initDatabase().then(async(db) => {
+  replicateRxCollection({
+    waitForLeadership : false,
+    collection: db.contacts,
+    replicationIdentifier: 'my-rest-replication-to-https://example.com/api/sync',
+    pull: {
+        /**
+         * Pull handler
+         */
+        async handler(lastCheckpoint, batchSize) {
+            console.log('lastCheckpoint', lastCheckpoint);
+            console.log('batchSize', batchSize)
+            return {
+                documents: [],
+                checkpoint: null
+            };
+        },
+        batchSize: 10,
+    },
+});
+
+});
